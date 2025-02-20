8b5a2f162a708ad74c86c84dcba8e71b9ec64b1b
milktea <yuribling123@gmail.com>
2025-02-20 13:25:40 -0800

    🚀 user button

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,14 @@
 "use client;"
+
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
-    return ( <div className="flex items-center gap-x-4 p-5 bg-green-500"> nav bar </div>  );
+    return ( <div className="flex items-center gap-x-4 p-5 bg-green-500"> 
+    <div className="hidden lg:flex lg:flex-1 bg-yellow-500">search</div>
+    
+    <UserButton></UserButton> 
+    
+    </div>  );
 }
  
 export default Navbar;
