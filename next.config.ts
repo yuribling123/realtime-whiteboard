dc07227649f25d6f03721cec795e474f26f2e767
milktea <yuribling123@gmail.com>
2025-02-28 20:38:25 -0800

    🚀 organizations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[{protocol:"https",hostname:"img.clerk.com"}]
+  }
 };
 
 export default nextConfig;
