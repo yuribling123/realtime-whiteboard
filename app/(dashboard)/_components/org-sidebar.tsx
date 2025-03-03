3ff5fbe52bd3d866deb4b3c7c7fa1f23819091b6
milktea <yuribling123@gmail.com>
2025-03-02 20:58:24 -0800

    ✨ logo

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,27 @@
 "use client"
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: "600",
+});
 
 const OrgSidebar = () => {
-    return ( <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500" >
-        Org Sidebar
-    </div> );
+    return (<div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-5" >
+        <Link href="/">
+            <div className="flex items-center gap-x-2">
+                <Image src="/logo.svg" alt="logo" height={60} width={60} ></Image>
+                <span className={cn("font-semibold text-2xl", font.className)}>
+                    Board
+                </span>
+
+
+            </div>
+        </Link>
+    </div>);
 }
- 
+
 export default OrgSidebar;
