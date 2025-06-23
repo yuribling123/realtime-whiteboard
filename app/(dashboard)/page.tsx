fea2f3d3a5566b80e793a6a9bb6cd8b03801b1a2
milktea <yuribling123@gmail.com>
2025-06-23 13:57:41 -0700

    ✨

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+import { useOrganization } from "@clerk/nextjs";
 import EmptyOrg from "./_components/empty-org";
 
 //sfc
 const DashBoardPage = () => {
+  const {organization} = useOrganization();
+
+
   return ( 
-  <div> 
+  <div className="flex-1 h-[cal(100%-80px)] p-6"> 
+  
     <EmptyOrg></EmptyOrg>
 
   </div> );
