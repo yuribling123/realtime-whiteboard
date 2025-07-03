5a29646dc53036d1f158fbdcf71c79e70ca5785c
milktea <yuribling123@gmail.com>
2025-07-02 22:58:30 -0700

    ⚡️

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
 const EmptyBoards = () => {
     return ( 
 
         <div className="h-full flex flex-col items-center justify-center">
+            <Image src="empty-boards.svg" height={140} width={140}  alt="Empty"/>
             <h2 className="text-2l font-semibold mt-6"> Create your first board</h2>
             <p className="text-muted-foreground text-sm mt-2"> Start creating a board for your organization </p>
 
