2a6db7bd6858496e230482fa165a558bed398f5b
milktea <yuribling123@gmail.com>
2025-07-17 22:03:39 -0700

    :bug:

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import Overlay from "./overlay";
  
 interface BoardCardProps{
     id:string;
@@ -30,7 +31,9 @@ const BoardCard = (
             <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
 
                 <div className="relative flex-1 bg-amber-50"> 
-                    <Image src={imageUrl} alt="doodle" fill className="object-fit"></Image>
+
+                    <Image src={imageUrl} alt={title} fill className="object-fit"></Image>
+                    <Overlay/>
 
                     <p className="pt-5">{imageUrl}</p>
                 </div>
