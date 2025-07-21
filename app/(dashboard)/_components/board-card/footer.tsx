952d71cc914a7dd5fc67f8010b1e08b6f9f16a95
milktea <yuribling123@gmail.com>
2025-07-20 18:23:29 -0700

    :construction:

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -23,6 +23,16 @@ const Footer = (
         <p className="opoacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate"> 
             {authorLabel},{createdAtLabel}
         </p>
+
+        <button disabled = {disabled}
+                onClick={onClick}
+                   className={cn("opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text=muted-foreground hover:text-blue-600",disabled &&"cursor-not-allowed opacity-75" )}             
+                >
+            <Star
+                className={cn("h-4 w-4", isFavorite && "fill-blue-600")}
+            />
+             
+        </button>
     </div> 
     );
 }
