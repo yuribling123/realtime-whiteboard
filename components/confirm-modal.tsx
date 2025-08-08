84bf505dffe5b69cc8161af553e866ba216cc5a2
milktea <yuribling123@gmail.com>
2025-08-08 10:02:05 -0700

    :sparkles:

--- components/confirm-modal.tsx
+++ components/confirm-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { AlertDialog } from "@radix-ui/react-alert-dialog";
-import { AlertDialogTrigger } from "./ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel } from "@radix-ui/react-alert-dialog";
+import { AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
 
 
 interface ConfirmModalProps {
@@ -14,11 +14,34 @@ interface ConfirmModalProps {
 const ConfirmModal = (
     {children, onConfirm, disabled, header, description}: ConfirmModalProps 
 ) => {
+    const handleConfirm = () => {
+        onConfirm
+    };
     return ( 
         <AlertDialog>
             <AlertDialogTrigger>
                     {children}
             </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>
+                        {header}
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                        {description}
+                    </AlertDialogDescription>      
+                </AlertDialogHeader>
+
+                <AlertDialogFooter>
+                    <AlertDialogCancel> Cancel</AlertDialogCancel>
+                    <AlertDialogAction disabled={disabled} onClick={handleConfirm}>
+                        Confirm
+                    </AlertDialogAction>
+
+                </AlertDialogFooter>
+
+
+            </AlertDialogContent>
         </AlertDialog>
      );
 }
