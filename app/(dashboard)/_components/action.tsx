67c54bc81589da3287e41947c5c17421fa47f589
milktea <yuribling123@gmail.com>
2025-08-10 13:09:50 -0700

    :bug:

--- app/(dashboard)/_components/action.tsx
+++ app/(dashboard)/_components/action.tsx
@@ -46,16 +46,17 @@ const Actions = (
             </DropdownMenuTrigger>
             <DropdownMenuContent onClick={(e)=>{e.preventDefault();e.stopPropagation()}} side={side}  sideOffset={sideoffset} className="w-60 z-50  bg-white rounded-md">
 
-                <DropdownMenuItem className="flex items-center cursor-pointer " onClick={onCopyLink}>
+                <DropdownMenuItem className="flex items-center p-3 w-full text-sm font-normal" onClick={onCopyLink}>
                     <Link2 className="h-4 w-4 mr-2 ml-2"/>
                     Copy Board Link
                 </DropdownMenuItem>
 
                 <ConfirmModal header="Delete Board" description="This will delete board and all of its contents." disabled={pending} onConfirm={onDelete}>
-                <Button variant="ghost" className="p-3 cursor-pointer w-full test-sm justify-start">
+                <Button variant="ghost" className="flex items-center  p-3 w-full text-sm font-normal">
                     <Trash2 className="h-4 w-4 mr-2 ml-2"/>
                     Delete
                 </Button>
+                
                 </ConfirmModal>
 
             </DropdownMenuContent>
