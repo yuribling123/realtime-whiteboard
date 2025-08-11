bf7cf7803ed0fe17c7268994586b314d6c22a34d
milktea <yuribling123@gmail.com>
2025-08-11 11:29:55 -0700

    :construction:

--- convex/board.ts
+++ convex/board.ts
@@ -43,3 +43,28 @@ export const remove = mutation({
     // or whatever you want to return
   },
 });  
+
+export const update = mutation({ 
+  args: {
+    id: v.id("boards"),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("unauthorized");
+    }
+    const title = args.title.trim();
+    if (!title) {
+      throw new Error("Title cannot be empty");
+    }
+    if (title.length > 60) {
+      throw new Error("Title cannot exceed 60 characters");
+    }
+
+     const board = await ctx.db.patch(args.id, {  
+      title:args.title
+    });
+    return board;
+  },
+})
