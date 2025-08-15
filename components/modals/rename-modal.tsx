3a0619d1256fac04a0e67fa00f340ffa5c79e57e
milktea <yuribling123@gmail.com>
2025-08-15 10:50:33 -0700

    :construction:

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -1,10 +1,21 @@
-import { Dialog } from "@radix-ui/react-dialog";
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { useRenameModal } from "@/store/use-rename-modal";
+import { DialogHeader } from "../ui/dialog";
 
 export const RenameModal = () => {
     const { isOpen, initialValues, onOpen, onClose } = useRenameModal();
     return (
     <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>
+                    Edit board Title
+                </DialogTitle>
+            </DialogHeader>
+            <DialogDescription>
+                Enter new title for the board.
+            </DialogDescription>
+        </DialogContent>
 
     </Dialog>)
 }
